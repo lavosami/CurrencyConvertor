ad1bd80f03181bd3402bf38a3c0722284d29da31
Lavosami <vap210504@gmail.com>
2023-05-22 00:05:49 +0300

    clear main section for tests

--- CurrencyConvertor/main.cpp
+++ CurrencyConvertor/main.cpp
@@ -9,19 +9,6 @@
 #include "Currency.hpp"
 
 int main(int argc, const char * argv[]) {
-    Currency currency = Currency("BYN", 5.23);
-    Currency currenc2 = Currency("BYN", 5.23);
-    
-    
-    currency.convert("RUB");
-    
-    currency = currency + currenc2;
-    
-    currency.print();
-    
-    currency = currency / 2;
-    
-    currency.print();
     
     return 0;
 }
